1259963ca0953ba23e6d6914bc2146a64f13854c
Melodie <47775467+melodiemia@users.noreply.github.com>
2022-10-14 12:37:55 +0100

    created app

---
+++ UnSoliciated Advice/UnSoliciated Advice/UnSoliciated_AdviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnSoliciated_AdviceApp.swift
+//  UnSoliciated Advice
+//
+//  Created by Melodie Trought on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnSoliciated_AdviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
